b3a00e4de5bacfd40479ba0c6d79eb1d5ef2478c
Chip McClelland <chip@mcclellands.org>
2017-12-13 18:12:00 -0600

    Updated comments and made some minor tweaks.

--- src/GPS-Tracker.ino
+++ src/GPS-Tracker.ino
@@ -5,9 +5,8 @@
  * Date: 12-12-17
  */
 
-
  // Variables I want to change often and pull them all together here
- #define SOFTWARERELEASENUMBER "0.75"
+ #define SOFTWARERELEASENUMBER "0.5"
 
  #include "Particle.h"
  #include "TinyGPS++.h"                       // https://github.com/mikalhart/TinyGPSPlus
@@ -24,179 +23,153 @@
 
  // Pin definitions
  const int enablePin = B4;      // Hold low to power down the device
- const int fixPin = B3;
- const int ledPin = D7;
+ const int fixPin = B3;         // From the GPS modlue - tracks the status "red" LED
+ const int ledPin = D7;         // To give a visual indication when a datapoint is recorded
 
  // Reporting intervals
- const unsigned long PUBLISH_PERIOD = 30000;     // How often will we report to Ubidots
- const unsigned long SERIAL_PERIOD = 5000;        // How often will we read the serial data
+ const unsigned long Ubidots_Frequency = 30000;     // How often will we report to Ubidots
  const unsigned long webhookWaitTime = 20000;     // How long will we wait for a webhook response
+ const unsigned long Particle_Frequency = 1000;     // Will limit how often we send updates
 
  // Program control valriables
- unsigned long lastSerial = 0;
+ unsigned long ubidotsPublish = 0;               // When was the last time we published
  unsigned long lastPublish = 0;
- unsigned long webhookTimeStamp = 0;
- unsigned long startFix = 0;
- bool gettingFix = false;
- bool dataInFlight = false;
- volatile bool fixFlag = false;             // Tracks fix interrupts
- unsigned long lastFixFlash = 0;            // when was the last flash
- bool gpsFix = false;                       // keeps track of our fix Status
- int resetCount = 0;
- int RSSI = 0;
+
+ unsigned long webhookTimeStamp = 0;              // When did we send the webhook
+ bool dataInFlight = false;                       // Tells us we send data but it has not yet been acknowledged by Ubidots
+ volatile bool fixFlag = false;                   // Tracks fixLED interrupts
+ unsigned long lastFixFlash = 0;                  // when was the last flash
+ bool gpsFix = false;                             // keeps track of our fix Status
+ int resetCount = 0;                              // Can track resets for future reporting
+ int errorCount = 0;                              // So we don't reset everytime a datapoint gets lost
+ int RSSI = 0;                                    // Signal strength in dBi
+ int gpsSamples = 0;                              // Counts the nubmer of samples received by Ubidots
 
  // Battery monitor
- int stateOfCharge = 0;                      // stores battery charge level value
+ int stateOfCharge = 0;                           // Stores battery charge level value
 
  //Menu and Program Variables
- const char* releaseNumber = SOFTWARERELEASENUMBER;  // Displays the release on the menu
- retained char Signal[17];             // Used to communicate Wireless RSSI and Description
- char Status[17] = "";                 // Used to communciate updates on System Status
+ const char* releaseNumber = SOFTWARERELEASENUMBER;   // Displays the release on the menu
+ retained char Signal[17];                            // Used to communicate Wireless RSSI and Description
+ char Status[17] = "";                                // Used to communciate updates on System Status
  const char* levels[6] = {"Poor", "Low", "Medium", "Good", "Very Good", "Great"};
-
 
  void setup()
  {
- 	Serial.begin(9600);                  // For debugging - removed for production
-  delay(3000);
   Serial1.begin(9600);                 // The GPS module is connected to Serial 1
-  pinMode(enablePin,OUTPUT);           // Can be used to turn the GPS on and off
+  pinMode(enablePin,OUTPUT);           // Can be used to turn the GPS on and off - not used in this version
   digitalWrite(enablePin,HIGH);        // Initially we will have the GPS on
-  pinMode(ledPin,OUTPUT);
+  pinMode(ledPin,OUTPUT);              // So we can signal when a datapoint is received
   digitalWrite(ledPin,LOW);
-  pinMode(fixPin,INPUT);
+  pinMode(fixPin,INPUT);               // Tied to the red indicator LED on the GPS Module
 
   char responseTopic[125];
   String deviceID = System.deviceID();                                // Multiple Electrons share the same hook - keeps things straight
   deviceID.toCharArray(responseTopic,125);
   Particle.subscribe(responseTopic, UbidotsHandler, MY_DEVICES);      // Subscribe to the integration response event
 
-  Particle.variable("Signal", Signal);
+  Particle.variable("Signal", Signal);                    // You will be able to monitor the device using the Particle mobile app
   Particle.variable("ResetCount", resetCount);
   Particle.variable("Release",releaseNumber);
   Particle.variable("stateOfChg", stateOfCharge);
+  Particle.variable("Samples",gpsSamples);
 
   Particle.function("Reset",resetNow);
 
-  attachInterrupt(fixPin,fixISR,RISING);                              // Going to see when we have a fix
+  attachInterrupt(fixPin,fixISR,RISING);                    // Going to see when we have a fix
   lastFixFlash = millis();
-  startFix = millis();
-  gettingFix = true;
 
-  Time.zone(-5);                                                        // Set time zone to Eastern USA daylight saving time
+  Time.zone(-5);                                            // Set time zone to Eastern USA daylight saving time
   takeMeasurements();
 
-  if (state != ERROR_STATE) {
-    state = IDLE_STATE;                         // IDLE unless error from above code
-    Serial.println("State - Idle");
-  }
-
+  state = IDLE_STATE;                                     // IDLE unless error from above code
+  waitUntil(meterParticlePublish);                       // Meter our Particle publishes
+  Particle.publish("State","Idle");
+  lastPublish = millis();
  }
 
  void loop()
  {
    switch(state) {
-   case IDLE_STATE: {
 
+   case IDLE_STATE:
      if (fixFlag) {
-       int difference = millis()-lastFixFlash;
-       fixFlag = false;  // Clear the fixFlag
-       if (difference >= 10000) {
-         gpsFix = true;           // Flashes every 15 sec when it has a fix
-         lastFixFlash = millis();
-         Serial.print("GPS Fix, difference: ");
-         Serial.println(difference);
-       }
-       else {
-         gpsFix = false;                                             // Flashes every second when it is looking
-         lastFixFlash = millis();
-         Serial.print("No fix, difference: ");
-         Serial.println(difference);
-       }
+       fixFlag = false;                                     // Clear the fixFlag
+       if (millis()-lastFixFlash >= 10000)  gpsFix = true;  // Flashes every 15 sec when it has a fix
+       else  gpsFix = false;                                // Flashes every second when it is looking
+       lastFixFlash = millis();                 // Reset the flag timer
      }
-     if ((millis() - lastPublish >= PUBLISH_PERIOD) && gpsFix) {
+     if ((millis() - ubidotsPublish >= Ubidots_Frequency) && gpsFix) {
        state = REPORTING_STATE;
-       Serial.println("State - Reporting");
+       waitUntil(meterParticlePublish);     // Meter our Particle publishes
+       Particle.publish("State","Reporting");
+       lastPublish = millis();
      }
-   } break;
+   break;
 
    case REPORTING_STATE:
      while (Serial1.available() > 0) {
        if (gps.encode(Serial1.read())) {
-         Serial.println("Serial data received");
          displayInfo();
          takeMeasurements();
          sendEvent();
          state = RESP_WAIT_STATE;                            // Wait for Response
-         Serial.println("State - Waiting for Response");
+         waitUntil(meterParticlePublish);     // Meter our Particle publishes
+         Particle.publish("State","Waiting for Response");
+         lastPublish = millis();
        }
      }
-     break;
+   break;
 
    case RESP_WAIT_STATE:
-
      if (!dataInFlight)                                  // Response received
      {
        state = IDLE_STATE;
-       Serial.println("State - Idle");
-       digitalWrite(ledPin,HIGH);
-       delay(1000);
+       waitUntil(meterParticlePublish);     // Meter our Particle publishes
+       Particle.publish("State","Idle");
+       lastPublish = millis();
+       digitalWrite(ledPin,HIGH);                         // So you can see when a datapoint is received
+       delay(2000);
        digitalWrite(ledPin,LOW);
      }
      else if (millis() >= (webhookTimeStamp + webhookWaitTime)) {
-       state = ERROR_STATE;  // Response timed out
-       Serial.println("State - Response Timeout Error");
+       state = ERROR_STATE;                               // Response timed out
+       waitUntil(meterParticlePublish);     // Meter our Particle publishes
+       Particle.publish("State","Response Timeout Error");
+       lastPublish = millis();
      }
-     break;
+   break;
 
-   case ERROR_STATE:                                          // To be enhanced - where we deal with errors
-     Serial.println("Error state - resetting Electron");
-     delay(1000);
-     //System.reset();                 // Today, only way out is reset
-     state = IDLE_STATE;
-     break;
+   case ERROR_STATE:                                      // To be enhanced - where we deal with errors
+     if (errorCount >= 3) {
+       waitUntil(meterParticlePublish);     // Meter our Particle publishes
+       Particle.publish("State","Too many errors - resetting Electron");
+       lastPublish = millis();
+       delay(3000);                                         // Give time to publish
+       System.reset();
+     }
+     errorCount++;                                      // Today, only way out is reset
+   break;
    }
  }
 
 void displayInfo()
 {
-  Serial.println("Display Info");
- 	if (millis() - lastSerial >= SERIAL_PERIOD) {
- 		lastSerial = millis();
- 		char buf[128];
- 		if (gps.location.isValid()) {
-      Serial.println("Valid GPS");
- 			snprintf(buf, sizeof(buf), "%f,%f,%f", gps.location.lat(), gps.location.lng(), gps.altitude.meters());
- 			if (gettingFix) {
- 				gettingFix = false;
- 				unsigned long elapsed = millis() - startFix;
- 				Serial.printlnf("%lu milliseconds to get GPS fix", elapsed);
- 			}
- 		}
- 		else {
- 			strcpy(buf, "no location");
- 			if (!gettingFix) {
- 				gettingFix = true;
- 				startFix = millis();
- 			}
- 		}
- 		Serial.println(buf);
-    if (Particle.connected()) {
-       lastPublish = millis();
-       Particle.publish("gps", buf);
-    }
- 	}
+  char buf[128];
+  if (gps.location.isValid()) snprintf(buf, sizeof(buf), "%f,%f,%f", gps.location.lat(), gps.location.lng(), gps.altitude.meters());
+  else strcpy(buf, "no location");
+  waitUntil(meterParticlePublish);     // Meter our Particle publishes
+  Particle.publish("gps", buf);
+  lastPublish = millis();
 }
 
 
 void sendEvent()
 {
   char data[256];                                         // Store the date in this character array - not global
-  float lat =
   snprintf(data, sizeof(data), "{\"battery\":%i, \"signal\":%i, \"lat\":%f, \"lng\":%f}",stateOfCharge, RSSI, gps.location.lat(), gps.location.lng());
-  Serial.println(data);
   Particle.publish("GPSlog_hook", data, PRIVATE);
-  webhookTimeStamp = millis();
+  ubidotsPublish = millis();
   dataInFlight = true; // set the data inflight flag
 }
 
@@ -204,17 +177,25 @@ void UbidotsHandler(const char *event, const char *data)  // Looks at the respon
 {
   // Response Template: "{{hourly.0.status_code}}"
   if (!data) {                                            // First check to see if there is any data
-    Particle.publish("Ubidots Hook", "No Data");
+    waitUntil(meterParticlePublish);     // Meter our Particle publishes
+    Particle.publish("Webhook Response","Empty");
+    lastPublish = millis();
     return;
   }
   int responseCode = atoi(data);                          // Response is only a single number thanks to Template
   if ((responseCode == 200) || (responseCode == 201))
   {
+    waitUntil(meterParticlePublish);     // Meter our Particle publishes
     Particle.publish("State","Response Received");
-    Serial.println("Response Received");
+    lastPublish = millis();
     dataInFlight = false;                                 // Data has been received
+    gpsSamples++;                                         // So you can see how many samples are recorded
   }
-  else Particle.publish("Ubidots Hook", data);             // Publish the response code
+  else {
+    waitUntil(meterParticlePublish);     // Meter our Particle publishes
+    Particle.publish("Webhook Response",data);
+    lastPublish = millis();
+  }
 }
 
 void getSignalStrength()
@@ -223,6 +204,9 @@ void getSignalStrength()
     RSSI = sig.rssi;
     int strength = map(RSSI, -131, -51, 0, 5);
     snprintf(Signal,17, "%s: %d", levels[strength], RSSI);
+    waitUntil(meterParticlePublish);     // Meter our Particle publishes
+    Particle.publish("Signal",Signal);
+    lastPublish = millis();
 }
 
 void takeMeasurements() {
@@ -243,4 +227,10 @@ int resetNow(String command)   // Will reset the Electron
 void fixISR()
 {
   fixFlag = true;
+}
+
+bool meterParticlePublish(void)
+{
+  if(millis() - lastPublish >= Particle_Frequency) return 1;
+  else return 0;
 }
